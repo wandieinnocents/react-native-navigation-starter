9cd8000ce3cd1f31cdba05a74ce765b9fdf49c2c
wandieinnocents <wandieinnocent2@gmail.com>
2022-02-09 04:20:34 -0800

    Formalt

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import { ContactStackNavigator,ServiceStackNavigator } from "./StackNavigator";
+import TabNavigator from "./TabNavigator";
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+    // customize drawer
+    screenOptions={{ headerShown: false }}
+    >
+      <Drawer.Screen name="HomeUI" component={TabNavigator} />
+      <Drawer.Screen name="ContactUI" component={ContactStackNavigator} />
+      <Drawer.Screen name="ServicesUI" component={ServiceStackNavigator} />
+    </Drawer.Navigator>
+  );
+}
+
+export default DrawerNavigator;
