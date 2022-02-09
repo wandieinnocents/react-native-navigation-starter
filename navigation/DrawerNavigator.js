e50e0e65dad4fefb4354c4724a635ab8ccb8fd21
wandieinnocents <wandieinnocent2@gmail.com>
2022-02-09 06:12:43 -0800

    Add icons on drawer navigation bar

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -2,7 +2,9 @@ import React from "react";
 
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import { ContactStackNavigator,ServiceStackNavigator } from "./StackNavigator";
+import { ContactStackNavigator, ServiceStackNavigator } from "./StackNavigator";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
 import TabNavigator from "./TabNavigator";
 
 const Drawer = createDrawerNavigator();
@@ -10,18 +12,77 @@ const Drawer = createDrawerNavigator();
 const DrawerNavigator = () => {
   return (
     <Drawer.Navigator
-    // customize drawer
-    screenOptions={{ headerShown: false }}
+      // customize drawer
+      screenOptions={{ headerShown: false }}
     >
-      <Drawer.Screen name="HomeUI" component={TabNavigator} />
-      <Drawer.Screen name="ContactUI" component={ContactStackNavigator} />
-      <Drawer.Screen name="ServicesUI" component={ServiceStackNavigator} />
-      <Drawer.Screen name="ProfileUI" component={ServiceStackNavigator} />
-      <Drawer.Screen name="HistoryUI" component={ServiceStackNavigator} />
-      <Drawer.Screen name="SuportUI" component={ServiceStackNavigator} />
-      
+      <Drawer.Screen
+        name="HomeUI"
+        component={TabNavigator}
+        // icon
+        options={{
+          title: "HomeUI",
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color="purple" size={26} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="ContactUI"
+        component={ContactStackNavigator}
+        // icon
+        options={{
+          title: "ContactUI",
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="phone" color="purple" size={26} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="ServicesUI"
+        component={ServiceStackNavigator}
+        // icon
+        options={{
+          title: "ServicesUI",
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color="purple" size={26} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="ProfileUI"
+        component={ServiceStackNavigator}
+        // icon
+        options={{
+          title: "ProfileUI",
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-circle" color="purple" size={26} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="HistoryUI"
+        component={ServiceStackNavigator}
+        // icon
+        options={{
+          title: "HistoryUI",
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="heart" color="purple" size={26} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="SuportUI"
+        component={ServiceStackNavigator}
+        // icon
+        options={{
+          title: "SuportUI",
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="car" color="purple" size={26} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
-}
+};
 
 export default DrawerNavigator;
