5f417c69c1e08a38238c6a2e23cc827a222ce9b1
wandieinnocents <wandieinnocent2@gmail.com>
2022-02-09 05:06:02 -0800

    Add vector icons

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,31 +1,70 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { MainStackNavigator, ContactStackNavigator , GalleryStackNavigator, ServiceStackNavigator} from "./StackNavigator";
+import {
+  MainStackNavigator,
+  ContactStackNavigator,
+  GalleryStackNavigator,
+  ServiceStackNavigator,
+} from "./StackNavigator";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator
-    screenOptions={{
-      tabBarStyle: [{ display: 'flex' }],
-      tabBarShowLabel: true,
-      tabBarLabelStyle: {
-        paddingBottom: 6,
-        color: '#333',
-      },
-    //   Hide header of tab bar
-      headerShown: false, 
-    }}
-    >
-      <Tab.Screen name="HomePage" component={MainStackNavigator} />
-      <Tab.Screen name="ContactPage" component={ContactStackNavigator} />
-      <Tab.Screen name="ServicesPage" component={ServiceStackNavigator} />
-      
-      <Tab.Screen name="GallerPAGE" component={GalleryStackNavigator} />
 
+    <Tab.Navigator 
+    screenOptions={{ headerShown: false  }}
+    labeled={false} 
+    barStyle={{ backgroundColor: 'black' }} 
+    
+    activeColor="white" >
+    {/* Screens */}
+      <Tab.Screen
+        name="HomePage"
+        component={MainStackNavigator} //Home Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
 
+      <Tab.Screen
+        name="ContactPage"
+        component={ContactStackNavigator} // Search Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={26} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="ServicesPage"
+        component={ServiceStackNavigator}
+        // Notification Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="heart" color={color} size={26} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="GallerPAGE"
+        component={GalleryStackNavigator} // Profile Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-circle"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
